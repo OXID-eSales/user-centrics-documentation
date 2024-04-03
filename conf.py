e302f34c51592fe3938b2ab11cd6eb4e355543f6
christoph albrecht <christoph.albrecht@oxid-esales.com>
2024-04-03 11:15:20 +0200

    OXDEV-7843: version switch corrected -- DE

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ html_context = {
     'versions':
          [('1.1', 'https://docs.oxid-esales.com/modules/usercentrics/de/1.1/'),
           ('1.2', 'https://docs.oxid-esales.com/modules/usercentrics/de/1.2/'),
-          ('2.0', 'https://docs.oxid-esales.com/modules/usercentrics/de/F/'),
+          ('2.0', 'https://docs.oxid-esales.com/modules/usercentrics/de/2.0/'),
           ('3.0', 'https://docs.oxid-esales.com/modules/usercentrics/de/3.0/')],
 #   'languages':
 #         [('DE', 'https://docs.oxid-esales.com/modules/usercentrics/de/3.0/'),
